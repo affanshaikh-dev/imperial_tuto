6617ac461b70407170ed541acec72b228d559ec2
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-01-04 18:39:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// universal JS code
+window.addEventListener('scroll', () => {
+    popupContainer.style.display = 'none';
+    galleryPopup.style.display = 'none';
+    navbar.classList.remove("active")
+})
+
 // navigation section -->
 let navbar = document.querySelector(".navbar");
 let navBtn = document.querySelector(".humberger");
@@ -18,7 +25,7 @@ counts.forEach((counter) => {
     function upData() {
         const target = Number(counter.getAttribute('data-target'));
         const count = Number(counter.innerText.replace('+', ''));
-        const inc = (target - count) / speed; 
+        const inc = (target - count) / speed;
 
         if (count !== target) {
             counter.innerText = `${Math.ceil(count + inc)}+`;
@@ -30,15 +37,16 @@ counts.forEach((counter) => {
     upData();
 });
 
-new Swiper('.swiper', {
+// courses swiper
+let boxSwiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 3,
     centerSlide: 'true',
     spaceBetween: 15,
     fade: 'true',
     autoplay: {
-        delay: 3000, 
-        disableOnInteraction: false, 
+        delay: 3000,
+        disableOnInteraction: false,
     },
     breakpoints: {
         0: {
@@ -51,4 +59,62 @@ new Swiper('.swiper', {
             slidesPerView: 3,
         },
     },
+});
+
+// highlight swiper
+let fullSwiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    fade: 'true',
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+})
+
+// highlight popup
+let highlightImages = document.querySelectorAll('.highlightImg');
+let popupContainer = document.getElementById('popupContainer');
+let popupImage = document.getElementById('popupImage');
+let closeBtn = document.querySelector('.closeBtn');
+
+highlightImages.forEach((img) => {
+    img.addEventListener('click', () => {
+        popupImage.src = img.src;
+        popupContainer.style.display = 'flex';
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+});
+
+popupContainer.addEventListener('click', (e) => {
+    if (e.target === popupContainer) {
+        popupContainer.style.display = 'none';
+    }
+});
+
+// gallery popup
+let glImages = document.querySelectorAll(".imgGal")
+let galleryPopup = document.querySelector("#galleryPopup")
+let glPopupImage = document.querySelector(".glPopupImage")
+let clsBtn = document.querySelector(".glCloseBtn")
+
+glImages.forEach((glImg) => {
+    glImg.addEventListener('click', () => {
+        glPopupImage.src = glImg.src;
+        galleryPopup.style.display = 'flex';
+    })
+})
+
+clsBtn.addEventListener('click', () => {
+    galleryPopup.style.display = 'none';
+})
+
+galleryPopup.addEventListener('click', (e) => {
+    if (e.target === galleryPopup) {
+        galleryPopup.style.display = 'none';
+    }
 });
